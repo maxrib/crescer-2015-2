a3679d28a73d602b96c1b9f7a941d5a5111b6972
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-09-16 13:15:25 -0300

    Exs 1 e 3

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,7 +1,20 @@
 public class Elfo {
     private String nome;
-    private int flechas;
-    
+    private int flechas,experiencia;
+    public int atirarFlecha(){
+        if(flechas==0){
+            return flechas;
+        }
+        else{
+            experiencia++;
+            flechas--;
+            return experiencia;
+        }
+    }
+    public Elfo(String n) {
+        nome = n;
+        flechas = 42;
+    }
     public Elfo(String n, int flech) {
         nome = n;
         flechas = flech;
