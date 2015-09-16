92d0fb680eb42ca8a26ec0df0e326bf0cf58fb30
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-09-16 17:34:09 -0300

    Up!

--- src/modulo-01-java-OO/Modulo01/Bluej/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Bluej/Elfo.java
@@ -1,32 +1,30 @@
 public class Elfo {
     private String nome;
     private int flechas,experiencia;
-    public int atirarFlecha(){
-        if(flechas==0){
-            return flechas;
-        }
-        else{
-            experiencia++;
-            flechas--;
-            return experiencia;
-        }
-    }
+    
+    public void atirarFlechaDwarf(Dwarf dwarf){
+        dwarf.receberFlechada();
+        experiencia++;
+        flechas--;
+        }    
+        
     public Elfo(String n) {
         nome = n;
         flechas = 42;
     }
+    
     public Elfo(String n, int flech) {
         nome = n;
         flechas = flech;
     }
-}
+    
     public String getNome() {
         return nome;
     }
     
-    public void setNome(String novoNome) {
+    /*public void setNome(String novoNome) {
         this.nome = novoNome;
-    }
+    }*/
     
     public int getFlechas() {
         return this.flechas;
