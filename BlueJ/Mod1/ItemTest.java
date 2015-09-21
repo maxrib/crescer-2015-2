4981b9cd61350ddd083e4543970f5c0e9ff6f5cd
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-09-21 14:15:47 -0300

    feat(ItemTest): testar quantidade de itens.

--- BlueJ/Mod1/ItemTest.java
+++ BlueJ/Mod1/ItemTest.java
@@ -10,4 +10,25 @@ public class ItemTest{
         Item item = new Item("null",0);
         assertEquals("null",item.getDesc());
     }
+    
+    @Test
+    public void verificarQtdDePoucosItens(){
+        Item item = new Item("facão",4);
+        assertEquals("facão",item.getDesc());
+        assertEquals(4,item.getQtd());
+    }
+    
+    @Test
+    public void verificarQtdDeMuitosItens(){
+        Item item = new Item("facão",100);
+        assertEquals("facão",item.getDesc());
+        assertEquals(100,item.getQtd());
+    }
+    
+    @Test
+    public void verificarQtdDeItens(){
+        Item item = new Item("facão",47);
+        assertEquals("facão",item.getDesc());
+        assertEquals(47,item.getQtd());
+    }
 }
