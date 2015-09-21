e919688ffdf5e430d6cc4f5444a813815aa0e36d
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-09-21 00:55:58 -0300

    feat(Item): criar classe Item

--- BlueJ/Mod1/Item.java
+++ BlueJ/Mod1/Item.java
@@ -7,11 +7,20 @@ public class Item{
         this.qtd=qtd;
     }
     
+    public Item(String desc){
+        this.desc=desc;
+        this.qtd=1;
+    }
+    
     public String getDesc(){
         return this.desc;
     }
     
     public int getQtd(){
         return this.qtd;
+    }
+    
+    public void setQtdLeprechaun(){
+        this.qtd += 1000;
     }
 }
