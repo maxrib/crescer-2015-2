6116a809a615fa27cba71e80f2cf15195838b5c2
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-09-21 14:16:46 -0300

    feat(InventarioTest): testar parâmetros do inventario.

---
+++ BlueJ/Mod1/InventarioTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InventarioTest{
+    @Test
+    public void conferirInventario(){
+        //Item item = new Item("boleadeira");
+        //Inventario inventario = new Inventario();
+        //inventario.adicionarItem(Item item);
+        //assertEquals("boleadeira.",inventario.getDescricoesItens());
+    }
+}
