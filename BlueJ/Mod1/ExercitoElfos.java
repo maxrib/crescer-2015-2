2e35231ca430b0a5e9c45a1366f179add9865a22
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-09-24 02:41:10 -0300

    Pesquisar elfo por nome

--- BlueJ/Mod1/ExercitoElfos.java
+++ BlueJ/Mod1/ExercitoElfos.java
@@ -1,14 +1,38 @@
 import java.util.ArrayList;
+import java.util.*;
+import java.util.HashMap;
 
 public class ExercitoElfos {
-    private ArrayList<Elfo> elfos = new ArrayList<>();
+    HashMap<Status,ArrayList<Elfo>> statusElfo;
+    HashMap<String,Elfo> exercitoElfos;
+    
+    public ExercitoElfos(){
+        exercitoElfos = new HashMap<>();
+    }
     
     public void alistarElfoVerde(ElfoVerde elfo) {
-        this.elfos.add(elfo);
+        exercitoElfos.put(elfo.getNome(),elfo);
     }
     
     public void alistarElfoNoturno(ElfoNoturno elfo) {
-        this.elfos.add(elfo);
+        exercitoElfos.put(elfo.getNome(),elfo);
     }
     
+    public int getTotalDeElfos()
+   {
+       return exercitoElfos.size();
+   }
+   
+   public Elfo pesquisarElfo(String nome)
+   {
+       if(exercitoElfos.containsKey(nome))
+       {
+           return exercitoElfos.get(nome);
+       }
+       else
+       {
+           return null;
+       }
+   }
+   
 }
