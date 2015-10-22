3f3a41f1267092e911c689dce515ed68ba62169a
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-10-22 00:07:55 -0200

    otimizar código.

--- src/modulo-03-web/dia-04/tema-carrinho-de-compras/String.js
+++ src/modulo-03-web/dia-04/tema-carrinho-de-compras/String.js
@@ -1,6 +1,6 @@
 //Verificar se é palíndromo
 String.prototype.palindromo = function () {
-    return this.toLowerCase().toString() === this.split("").reverse().join("").toLowerCase().toString();;
+    return this.toLowerCase().toString() === this.split("").reverse().join("").toLowerCase();
 };
 
 //Verificar se começa com a letra 'B'
