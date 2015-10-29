09635f5e0d7259491e6ad8721dae49e3e00c15b7
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-10-28 23:20:12 -0200

    Locadora - cadastrar novo jogo

---
+++ src/tarefas-modulo-04/Locadora/Locadora.Dominio/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Locadora.Dominio
+{
+    public class Dominio
+    {
+        private const string caminho = @"C:\Users\Ale\Google Drive\git\crescer-2015-2\src\tarefas-modulo-04\Locadora\game_store.xml";
+
+        public static void CadastrarJogo(string nome,float preco, string categoria)
+        {
+            XElement locadoraXML = XElement.Load(caminho);
+            
+            int ultimoId = Convert.ToInt32(locadoraXML.Elements("jogo").Last().Attribute("id").Value);
+
+            XElement cadastro = new XElement("jogo",
+                new XElement("nome", nome),
+                new XElement("preco", preco),
+                new XElement("categoria", categoria));
+
+            cadastro.SetAttributeValue("id", ultimoId + 1);
+
+            locadoraXML.Add(cadastro);
+
+            locadoraXML.Save(caminho);
+        }
+
+        public static void PesquisarJogos()
+        {
+            Console.WriteLine("Pesquisando jogos...");
+        }
+        public static void EditarJogo()
+        {
+            Console.WriteLine("Editando jogo...");
+        }
+        public static void ExportarRelatorio()
+        {
+            Console.WriteLine("Exportando relatório...");
+        }
+    }
+}
---
+++ src/tarefas-modulo-04/Locadora/Locadora.UI/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Locadora.UI
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.SetCursorPosition(10, 3);
+            ushort option = 0;
+
+            while (option != 5)
+            {
+                Console.Clear();
+                Console.WriteLine("Locadora Nunes Games");
+                Console.WriteLine("====================");
+                Console.WriteLine("Selecione uma das Opções:");
+                Console.WriteLine("1. Cadastrar um novo Jogo\n2. Pesquisar Jogos\n3. Editar um jogo\n4. Exportar relatório de jogos\n5. Sair");
+                option = ushort.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Digite o nome do jogo:");
+                        var nome = Console.ReadLine();
+                        Console.WriteLine("Digite o preço do jogo: ");
+                        var preco = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite a categoria do jogo: ");
+                        var categoria = Console.ReadLine();
+                        Locadora.Dominio.Dominio.CadastrarJogo(nome,preco,categoria);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Locadora.Dominio.Dominio.PesquisarJogos();
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Locadora.Dominio.Dominio.EditarJogo();
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Locadora.Dominio.Dominio.ExportarRelatorio();
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Esta não é uma opção válida. Tente Novamente.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
