e600df276a3f6a6179e9f54394691e91cdcf9144
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-10-28 23:18:41 -0200

    aula 3 - trabalho com arquivos

---
+++ src/tarefas-modulo-04/TrabalhoComArquivos/TrabalhoComArquivos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoComArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+
+        }
+
+
+        /*
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Users\Ale\Desktop\Arquivos\meu-arquivo.txt";
+            string textoDoArquivo = "didi must die\n";
+            //File.AppendAllText(caminhoArquivo, textoDoArquivo);
+            //var linhas = File.ReadAllLines(caminhoArquivo);
+            //string[] teste = File.ReadAllLines(caminhoArquivo);
+            //string texto = File.ReadAllText(caminhoArquivo);
+            //string newLine = Environment.NewLine;
+            byte[] arquivo = File.
+            Console.Read();
+
+            string caminhoArquivo = @"C:\Users\Ale\Desktop\Arquivos\meu-arquivo.txt";
+            var writer = new StreamWriter(caminhoArquivo, true);
+            writer.WriteLine("xuxa must die\n");
+            writer.Close();
+            Console.Read();
+
+
+            string caminhoArquivo = @"C:\Users\Ale\Desktop\Arquivos\meu-arquivo.txt";
+            var arquivoPrnciapl = File.Open(caminhoArquivo,FileMode.Open, FileMode.)
+            var reader = new StreamReader(caminhoArquivo);
+            var leitura = reader.ReadToEnd();
+            Console.Read();
+
+            tring caminhoArquivo = @"C:\Users\Ale\Desktop\Arquivos\meu-arquivo.txt";
+            using (var reader = new StreamReader(caminhoArquivo))
+            {
+                reader.ReadLine();
+            }
+            Console.Read();
+        }
+        */
+    }
+}
