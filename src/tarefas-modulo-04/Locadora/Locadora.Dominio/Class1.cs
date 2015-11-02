a2eeda33a8a9d2748b4b48a739a0ee4eb60a4b86
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-11-02 01:26:17 -0200

    locadora - criar método para editar jogo.

--- src/tarefas-modulo-04/Locadora/Locadora.Dominio/Class1.cs
+++ src/tarefas-modulo-04/Locadora/Locadora.Dominio/Class1.cs
@@ -52,10 +52,49 @@ namespace Locadora.Dominio
             }
         }
 
-        public static void EditarJogo()
+        public static void EditarJogo(int idJogo)
         {
-            Console.WriteLine("Editando jogo...");
+            XElement locadoraXML = XElement.Load(caminho);
+
+            IEnumerable<XElement> buscaJogo =
+                from el in locadoraXML.Elements("jogo")
+                where (int)el.Attribute("id") == idJogo
+                select el;
+
+            if (buscaJogo.Count() >= 1)
+            {
+                foreach (XElement el in buscaJogo)
+                {
+                    Console.SetCursorPosition(3, 3);
+                    Console.WriteLine("Nome     : "+ el.Element("nome").Value);
+                    Console.SetCursorPosition(3, 5);
+                    Console.WriteLine("Preço    : " + el.Element("preco").Value);
+                    Console.SetCursorPosition(3, 7);
+                    Console.WriteLine("Categoria: " + el.Element("categoria").Value);
+
+                    Console.SetCursorPosition(40, 3);
+                    Console.Write("Novo nome     : ");
+                    var novoNome = Console.ReadLine();
+                    Console.SetCursorPosition(40, 5);
+                    Console.Write("Novo preço    : ");
+                    var novoPreco = Console.ReadLine();
+                    Console.SetCursorPosition(40, 7);
+                    Console.Write("Nova categoria: ");
+                    var novaCategoria = Console.ReadLine();
+
+                    el.Element("nome").Value = novoNome;
+                    el.Element("preco").Value = novoPreco;
+                    el.Element("categoria").Value = novaCategoria;
+
+                    locadoraXML.Save(caminho);
+                }
+            }
+            else
+            {
+                Console.Write("ID não existe!");
+            }
         }
+
         public static void ExportarRelatorio()
         {
             Console.WriteLine("Exportando relatório...");
