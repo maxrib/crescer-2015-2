080d589a4cab3023e659473daefe5062b82fab51
Maxoel Ribeiro <maxoel.ribeiro@gmail.com>
2015-11-02 19:43:48 -0200

    emitir relatório - locadora

--- src/tarefas-modulo-04/Locadora/Locadora.Dominio/Class1.cs
+++ src/tarefas-modulo-04/Locadora/Locadora.Dominio/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -72,13 +73,13 @@ namespace Locadora.Dominio
                     Console.SetCursorPosition(3, 7);
                     Console.WriteLine("Categoria: " + el.Element("categoria").Value);
 
-                    Console.SetCursorPosition(40, 3);
+                    Console.SetCursorPosition(3, 10);
                     Console.Write("Novo nome     : ");
                     var novoNome = Console.ReadLine();
-                    Console.SetCursorPosition(40, 5);
+                    Console.SetCursorPosition(3, 12);
                     Console.Write("Novo preço    : ");
                     var novoPreco = Console.ReadLine();
-                    Console.SetCursorPosition(40, 7);
+                    Console.SetCursorPosition(3, 14);
                     Console.Write("Nova categoria: ");
                     var novaCategoria = Console.ReadLine();
 
@@ -97,7 +98,56 @@ namespace Locadora.Dominio
 
         public static void ExportarRelatorio()
         {
-            Console.WriteLine("Exportando relatório...");
+            XElement locadoraXML = XElement.Load(caminho);
+
+            IEnumerable<XElement> buscaJogo =
+                from el in locadoraXML.Elements("jogo")
+                select el;
+
+            string reportPath = @"C:\GamesReport.txt";
+
+            var writer = new StreamWriter(reportPath, true);
+
+            var dt = string.Format("{0:dd/MM/yyyy}", DateTime.Now);
+            var hr = string.Format("{0:HH:mm:ss}", DateTime.Now);
+
+            writer.WriteLine("                             LOCADORA NUNES GAMES                               ");
+            writer.WriteLine("{0}                                                              {1}", dt, hr);
+            writer.WriteLine("                              Relatório de jogos                                \n\n");
+            writer.WriteLine("================================================================================");
+            writer.WriteLine("ID       Categoria        Nome                          Preço         Disponivel");
+
+            foreach (XElement el in buscaJogo)
+            {
+                var gameId = el.Attribute("id").Value.ToString();
+                var spcCat = string.Join("", Enumerable.Repeat(' ', 9 - gameId.Length));
+                var gameCategoria = el.Element("categoria").Value.ToString();
+                var spcNome = string.Join("", Enumerable.Repeat(' ', 17 - gameCategoria.Length));
+                var gameNome = el.Element("nome").Value.ToString();
+                var spcPreco = string.Join("", Enumerable.Repeat(' ', 30 - gameNome.Length));
+                var gamePreco = el.Element("preco").Value.ToString();
+                var spcDisp = string.Join("", Enumerable.Repeat(' ', 10 - gamePreco.Length));
+
+                writer.WriteLine("{0}{1}{2}{3}{4}{5}R$ {6}{7} Sim", gameId, spcCat, gameCategoria, spcNome, gameNome, spcPreco, gamePreco, spcDisp);
+            }
+
+            var qtdJogos = buscaJogo.Count();
+            var vlrMedio = buscaJogo.Average(el => float.Parse(el.Element("preco").Value));
+
+            //var maisCaro = buscaJogo.Where(x => x.Element("preco").Value == buscaJogo.Max(b => b.Element("preco").Value)).Elements("jogo");
+            //var maisBarato = buscaJogo.Select(el => el.Element("nome")).Min(el => el.Element("preco").Value);
+
+            writer.WriteLine("Quantidade total de jogos: {0}", qtdJogos);
+            writer.WriteLine("Quantidade de jogos disponíveis: {0}", qtdJogos);
+            //Console.WriteLine("Valor médio por jogo: R$ {0}", vlrMedio);
+            //Console.WriteLine("Jogo mais caro: ", maisCaro);
+            //Console.WriteLine("Jogo mais barato: ", maisBarato);
+
+
+            writer.Close();
+
+            Console.Read();
+
         }
     }
 }
